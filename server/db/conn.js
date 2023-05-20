45c397e7fb9b3957ccececf6714c86b45afc2238
Thamitha <thamitharajput@gmail.com>
2023-05-20 19:11:12 +0530

    added functionality signIn and signUp

--- server/db/conn.js
+++ server/db/conn.js
@@ -5,9 +5,7 @@ const DB = process.env.DATABASE;
 mongoose.connect(DB,{
     useUnifiedTopology:true,
     useNewUrlParser:true
-}).then(()=>{
-    console.log("Database Connected")
-})
+}).then(()=>console.log("Database Connected"))
 .catch((error)=>{
     console.log("error",error);
 })
--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const validator =require("validator");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     fname: {
@@ -25,3 +26,17 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+
+//hash password
+userSchema.pre("save",async function(next){
+    if(this.isModified("password")){
+        this.password = await bcrypt.hash(this.password,12);
+    }
+    next();
+})
+
+//creating model
+
+const users=new mongoose.model("thamitha",userSchema);
+
+module.exports = users;
