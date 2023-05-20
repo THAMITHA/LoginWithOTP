9c1e79880384d75d17c995c8a0673933b9c93e33
Thamitha <thamitharajput@gmail.com>
2023-05-21 03:18:01 +0530

    Login with OTP

--- server/Routes/router.js
+++ server/Routes/router.js
@@ -5,5 +5,6 @@ const controllers = require("../controllers/userControllers");
 
 //Routes
 router.post("/user/register",controllers.userregister);
-
+router.post("/user/sendotp",controllers.userOtpSend);
+router.post("/user/login",controllers.userLogin);
 module.exports = router;
--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { response } = require("express");
+const SECRECT_KEY = "abcdefghijklmnop"
 
 const userSchema = new mongoose.Schema({
     fname: {
@@ -23,7 +26,16 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true,
         minlength:6
-    }
+    },
+    tokens:[
+        {
+            token:{
+                type:String,
+                required:true,
+                        
+            }
+        }
+    ]
 });
 
 
@@ -34,6 +46,21 @@ userSchema.pre("save",async function(next){
     }
     next();
 })
+
+//token generate
+userSchema.methods.generateAuthtoken = async function(){
+    try{
+        let newtoken = jwt.sign({_id:this._id},SECRECT_KEY,{
+            expiresIn:"1d"
+        });
+        this.tokens = this.tokens.concat({token:newtoken});
+        await this.save();
+        return newtoken; 
+    }
+    catch(error){
+        res.status(400).json(error)
+    }
+}
 
 //creating model
 
